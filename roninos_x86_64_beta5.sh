4227a7207eda654b7a33c19a476e303e462135dc
Albercoin <68326029+albercoin@users.noreply.github.com>
2024-01-24 23:04:05 +0100

    Update roninos_x86_64_beta5.sh

--- roninos_x86_64_beta5.sh
+++ roninos_x86_64_beta5.sh
@@ -117,8 +117,19 @@ _prep_install(){
     # install docker
     # Add Docker's official GPG key:
     apt-get update
-    apt-get install -y docker docker-compose
-
+    apt-get install ca-certificates curl gnupg
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
+	
+	# Add the repository to Apt sources:
+	echo \
+	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+	  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+	  tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    
     # install docker-compose
     curl -L https://github.com/docker/compose/releases/download/v2.21.0/docker-compose-linux-x86_64 -o /usr/bin/docker-compose    
     chmod +x /usr/bin/docker-compose
